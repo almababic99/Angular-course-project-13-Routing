f5ee1809e357cb7859c68c234555695899f8c3d2
Alma Babic <alma.babic@tacta.io>
2025-01-03 16:15:40 +0100

    Section 14 - Lecture 269

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -6,6 +6,7 @@
 import { Routes } from '@angular/router';
 import { TasksComponent } from './tasks/tasks.component';
 import { NoTaskComponent } from './tasks/no-task/no-task.component';
+import { UserTasksComponent } from './users/user-tasks/user-tasks.component';
 
 export const routes: Routes = [ 
   // Routes -> Angular type that defines an array of route objects, where each object represents a route in the application.
@@ -17,9 +18,18 @@ export const routes: Routes = [
     component: NoTaskComponent,
     // When the user navigates to localhost:4200/, the NoTaskComponent will be rendered.
   },
+//   {
+//     path: 'tasks',   // localhost:4200/tasks
+//     component: TasksComponent,
+//     // When the user navigates to localhost:4200/tasks, the TasksComponent will be rendered.
+//   },
   {
-    path: 'tasks',   // localhost:4200/tasks
-    component: TasksComponent,
-    // When the user navigates to localhost:4200/tasks, the TasksComponent will be rendered.
-  },
+    path: 'users/:userId',  // localhost:4200/users/<userId>
+    component: UserTasksComponent
+    // This route is dynamic because of the : in the URL path. The :userId part of the path indicates a parameter. 
+    // When a user navigates to a URL like localhost:4200/users/123, the part after users/ (in this case, 123) will be captured as the userId.
+    // This dynamic route is useful when you need to display content related to a specific user, based on their unique ID.
+    // This specifies that when the user navigates to a URL like localhost:4200/users/123, 
+    // Angular will render the UserTasksComponent, passing the userId 123 as a parameter.
+  }
 ];
--- src/app/users/user/user.component.ts
+++ src/app/users/user/user.component.ts
@@ -1,15 +1,23 @@
 import { Component, computed, input } from '@angular/core';
 
 import { type User } from './user.model';
+import { RouterLink, RouterLinkActive } from '@angular/router';
 
 @Component({
   selector: 'app-user',
   standalone: true,
+  imports: [RouterLink, RouterLinkActive],
   templateUrl: './user.component.html',
   styleUrl: './user.component.css',
 })
 export class UserComponent {
   user = input.required<User>();
+  // input.required<User>() means that this component expects an input property (user) of type User. 
+  // The required decorator indicates that this input is mandatory, and the component cannot function properly without it.
 
   imagePath = computed(() => 'users/' + this.user().avatar);
+  // computed allows you to define computed properties in a reactive way. 
+  // This ensures that whenever the user input changes, the computed property imagePath will automatically update.
+  // The computed property imagePath generates a URL to an image file for the user. It concatenates 'users/' with the user's avatar property 
+  // (e.g., if the user’s avatar is "avatar123.png", the result would be "users/avatar123.png").
 }
